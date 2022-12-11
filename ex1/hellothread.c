d717a19e2305b23b290ef336ca793e9cf3cfb5fc
rlawns0327 <rlawns0327@naver.com>
2022-12-12 07:31:49 +0900

    hellothread

---
+++ ex1/hellothread.c
@@ -0,0 +1,22 @@
+/* hellothread.c */
+/* pthread create example */
+#include <stdio.h>
+#include <pthread.h>
+
+void *hello_thread (void *arg)
+{
+	printf ("Thread: Hello, World!\n");
+	return arg;
+}
+int main()
+{
+	pthread_t tid;
+	int status;
+	/* 쓰레드 생성 */
+	status = pthread_create (&tid, NULL, hello_thread, NULL);
+	if (status != 0)
+		perror ("Create thread");
+	pthread_exit (NULL);
+	
+return 0;
+}
